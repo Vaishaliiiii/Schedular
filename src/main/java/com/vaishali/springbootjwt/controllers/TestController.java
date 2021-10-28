dd357590299d90450a8caa83d1cf77b632c3a9d6
Vaishaliiiii <vaishali.r.sharma@impetus.com>
2021-10-28 16:25:43 +0530

    jwt code

---
+++ src/main/java/com/vaishali/springbootjwt/controllers/TestController.java
@@ -0,0 +1,35 @@
+package com.vaishali.springbootjwt.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+	@GetMapping("/all")
+	public String allAccess() {
+		return "Public Content.";
+	}
+	
+	@GetMapping("/user")
+	@PreAuthorize("hasRole('USER') or hasRole('UNDERWRITER') or hasRole('ADMIN')")
+	public String userAccess() {
+		return "User Content.";
+	}
+
+	@GetMapping("/writer")
+	@PreAuthorize("hasRole('UNDERWRITER')")
+	public String moderatorAccess() {
+		return "Underwriter Board.";
+	}
+
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String adminAccess() {
+		return "Admin Board.";
+	}
+}
---
+++ src/main/java/com/vaishali/springbootjwt/models/ERole.java
@@ -0,0 +1,7 @@
+package com.vaishali.springbootjwt.models;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_UNDERWRITER,
+    ROLE_ADMIN
+}
